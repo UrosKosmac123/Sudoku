f5341f63f8e34d88e7422398b6342860b2bd1053
Uroš Kosmač <uros.kosmac@icloud.com>
2020-07-22 14:11:04 +0200

    Zaključena SudokuIgra in SudokuTabela

--- model.py
+++ model.py
@@ -11,9 +11,9 @@ class Sudoku_Napake:
 class Sudoku_Tabela:
 
     def __init__(self, datoteka_tabele):
-        self.tabela = __vstvari_tabelo(datoteka_tabele)
+        self.tabela = self.__vstvari_tabelo(datoteka_tabele)
 
-    def vstvari_tabelo(self, datoteka_tabele):
+    def __vstvari_tabelo(self, datoteka_tabele):
         #Sudoku tabela kot matrika
         tabela = []
 
@@ -45,7 +45,47 @@ class SudokuIgra:
         self.datoteka_tabele = datoteka_tabele
         self.zacni_igro = Sudoku_Tabela(datoteka_tabele).tabela
 
-    #def start(self):
-        #self.konec_igre = False
-        #self.uganka = []i
-        #for i in range(9):
+    def start(self):
+        self.konec_igre = False
+        self.uganka = []
+        for i in range(9):
+            self.uganka.append([])
+            for j in range(9):
+                self.uganka[i].append(self.start_puzzle[i][j])
+
+    def preveri_zmago(self):
+        for vrstica in range(9):
+            if not __self.preveri_vrstico(vrstica):
+                return False
+        for stolpec in  range(9):
+            if not __self.preveri_stolpec(stolpec):
+                return False
+        for vrstica in range(3):
+            for stolpec in range(3):
+                if not __self.preveri_3x3_blok(vrstica, stolpec):
+                    return False
+        
+        self.konec_igre = True
+        return True
+
+    def __preveri_blok_stevil(self, blok):
+        return set(blok) == set(range(1,10))
+
+    def __preveri_vrstico(self, vrstica):
+        return __preveri_blok_stevil(self.uganka[vrstica])
+
+    def __preveri_stolpec(self, stolpec):
+        return __preveri_blok_stevil(
+            [self.uganka[vrstica][stolpec] for vrstica in range(9)])
+
+    def __preveri_3x3_blok(self, vrstica, stolpec):
+        return __preveri_blok_stevil(
+            [
+                self.uganka[i][j]
+                for i in range((3*vrstica, 3*(vrstica+1))
+                for j in range(3*stolpec, 3*(stolpec+1))
+            ]
+        )
+            
+
+
